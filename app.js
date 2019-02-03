a9b7a3fdca940c7ac75ba21b856c59f7b976e5c7
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-02 23:19:46 -0200

    Configurando porta do socket.io

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let app = require('./config/server');
 
-app.listen(3001, function(){
+let server = app.listen(3001, function(){
     console.log('Servidore Online');
 });
+
+require('socket.io').listen(server);
