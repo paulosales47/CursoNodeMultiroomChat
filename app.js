0286046bcb507e9eb99201147cc2a56f2614e325
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-03 06:15:00 -0200

    Utilizando a função connect e disconnet para verificar quando o usuário acessa a pagina de chat

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+let socketIo = require('socket.io')
 let app = require('./config/server');
 
 let server = app.listen(3001, function(){
     console.log('Servidore Online');
 });
 
-require('socket.io').listen(server);
+let io = socketIo.listen(server);
+
+io.on('connect', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
+});
